7cffdbcd84beca6872700a8b74877dfe32acfa7f
Sami Kuikka <sami.kuikka@aalto.fi>
2023-01-08 12:20:30 +0200

    deleted cors

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 4001;
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000'
-    }
+    
 });
 
 io.on('connection', (socket) => {
